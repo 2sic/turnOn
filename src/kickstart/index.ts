ef8cbdda613df64416150da0ccd0fd3355874053
ijungleboy <daniel.mettler@2sic.com>
2021-01-28 15:28:07 +0100

    rename to turn-on

--- src/kickstart/index.ts
+++ src/kickstart/index.ts
@@ -1,7 +1,7 @@
 
 export * from './condition-type';
 export * from './condition-maker';
-export * from './kickstart';
+export * from './turn-on';
 export * from './is-loaded';
 export * from './settings';
 export * from './status';
