533ab83047a223a54af52c6918ec2da0c8f91e86
iJungleboy <daniel.mettler@2sic.com>
2024-06-11 17:03:22 +0200

    minor non-change

--- src/tags/config-tag-manager.ts
+++ src/tags/config-tag-manager.ts
@@ -6,34 +6,31 @@ import { log } from '../debug';
 import { convertConfigToTurnOn } from './config-to-turn-on';
 import { TurnOnRoot } from '../turnOn/turn-on-root';
 
-
 export class ConfigTagManager {
-  
   public tags = new Array<ConfigTag>();
 
   constructor(public root: TurnOnRoot) {}
 
-
   tryToLoadTag(node: HTMLElement): void {
     // Get config and skip if not relevant, or skip if already marked as in the queue
     const attr = node?.getAttribute?.(AttributeTurnOn);
-    if(!attr) return;
+    if (!attr) return;
     const skip = node?.getAttribute?.(AttributeSkip);
     log('skip', skip);
-    if(skip) return log('skip');
+    if (skip) return log('skip');
 
     log('attr', attr);
     const config = ConfigHelper.load(attr);
-    if(config.progress === ProgressError) {
+    if (config.progress === ProgressError) {
       console.error(config.error, node, attr);
       return;
     }
-    log('stable config')
+    log('stable config');
     this.add(node, config);
   }
-  
+
   /**
-   * 
+   *
    */
   add(node: HTMLElement, config: TurnOnConfigInternal): void {
     log('add', node, config);
@@ -44,7 +41,6 @@ export class ConfigTagManager {
 
   updateTags(): void {
     log(`updateTags: ${this.tags.length}`);
-    this.tags.forEach((t) => t.syncDom())
+    this.tags.forEach((t) => t.syncDom());
   }
-
 }
--- src/tags/tag-loader.ts
+++ src/tags/tag-loader.ts
@@ -2,7 +2,7 @@ import { AttributeSkip, AttributeTurnOn } from '../constants';
 import { log } from '../debug';
 import { ConfigTagManager } from './config-tag-manager';
 
-
+/** Query to find all tags that are not processed yet; processed will be marked with turn-on-skip */
 const queryForUnprocessedTags = `[${AttributeTurnOn}]:not([${AttributeSkip}])`;
 
 const ELEMENT_NODE = 1; // see https://developer.mozilla.org/en-US/docs/Web/API/Node
@@ -11,58 +11,72 @@ const ELEMENT_NODE = 1; // see https://developer.mozilla.org/en-US/docs/Web/API/
  * In charge of loading all turn-on tags from the DOM, both at first load as well as on DOM changes
  */
 export class TagLoader {
-
   /** Options for the observer (which mutations to observe) */
-  public config : MutationObserverInit = { 
-    attributes: false, 
-    childList: true, 
-    subtree: true 
+  private config: MutationObserverInit = {
+    attributes: false,
+    childList: true,
+    subtree: true,
   };
 
   /** The observer, in case we need to debug */
   public observer: MutationObserver;
 
   constructor(public tagManager: ConfigTagManager) {
-    this.scanExistingDom();
+    this.scanInitialDom();
     this.activateObserver();
   }
 
-  public scanExistingDom(): void {
-    log('scanExistingDom');
-    if(document.documentElement)
+  /** Scan initial dom */
+  private scanInitialDom(): void {
+    log('scanInitialDom');
+    if (document.documentElement)
       this.checkAndLoadChildren(document.documentElement);
   }
 
   private checkAndLoadChildren(parent: HTMLElement) {
     log('checkAndLoadChildren', parent);
-    if(!parent?.children?.length) return;
+    if (!parent?.children?.length) return;
     const tags = parent.querySelectorAll(queryForUnprocessedTags);
     tags.forEach((t: HTMLElement) => this.tagManager.tryToLoadTag(t));
   }
 
   public activateObserver(): void {
     log('activateObserver');
-  
+
     this.observer = new MutationObserver((mutations) => {
-      log('mutations detected', mutations);
+      // const additions = true;
+      // const changes = (window as any).turnOnWatchChanges ?? false;
+      // log(`mutations detected, processing additions=${additions} and window.turnOnWatchChanges=${changes}`, mutations);
+      log(`mutations detected`, mutations);
+
       // Loop through each changed item, check if it's something we want to initialize
       mutations.forEach((m) => {
         // Nodes added - let's check if it is a turn-on
-        if(m.type != 'childList') return;
+        if (m.type != 'childList') return;
         log('childList changes');
 
+        // if (additions)
         Array.from(m.addedNodes)
           // nodes can contain text and stuff as well, so we must filter first
-          .filter(n => n.nodeType === ELEMENT_NODE)
+          .filter((n) => n.nodeType === ELEMENT_NODE)
           .forEach((node: HTMLElement) => {
-            if(node?.getAttribute?.(AttributeTurnOn))
+            if (node?.getAttribute?.(AttributeTurnOn))
               this.tagManager.tryToLoadTag(node);
-            else 
-              this.checkAndLoadChildren(node);
+            else this.checkAndLoadChildren(node);
           });
+
+        // if (changes)
+        //   Array.from(m.addedNodes)
+        //     // nodes can contain text and stuff as well, so we must filter first
+        //     .filter((n) => n.nodeType === ELEMENT_NODE)
+        //     .forEach((node: HTMLElement) => {
+        //       if (node?.getAttribute?.(AttributeTurnOn))
+        //         this.tagManager.tryToLoadTag(node);
+        //       else this.checkAndLoadChildren(node);
+        //     });
       });
     });
-  
+
     // observe document for tags which include this. ATM don't observe header
     this.observer.observe(document.documentElement, this.config);
   }
